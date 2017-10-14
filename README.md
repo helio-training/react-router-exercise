87b9acbd855d7afad3e7747c2b6e402157b8ade9
Tyler Garlick <tjgarlick@gmail.com>
2017-10-14 10:39:47 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# React Router Exercise
+
+> Building an application using routing, or urls
+
+## Introduction
+
+React applications are usually built around urls.  We build up these urls to represent different pages within our applications.  Until now, we've built applications simply by creating components and placing them inline.
+
+As we start building out larger applications we'll want to create a `path` for different pages.  For example, we may want the `/orders` path to display all the user's different orders.  This process is called routing.
+
+In React, one of the most common libraries to use is: [React Router](https://reacttraining.com/react-router/web).  To understand how this works we're going to break down all the different routes and components.
+
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div className="App-intro">
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
